6c7d31664da98559a91f62085d71dcff19117c52
William Minshew <wminshew@gmail.com>
2020-02-16 11:25:42 -0800

    added spacemacs

---
+++ .spacemacs.d/init.el
@@ -0,0 +1,5 @@
+(add-to-list 'default-frame-alist
+             '(ns-transparent-titlebar . t))
+
+(add-to-list 'default-frame-alist
+             '(ns-appearance . dark)) ;; or dark - depending on your theme
